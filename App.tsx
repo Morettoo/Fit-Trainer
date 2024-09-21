b2105f8eaf0acd68c5da569e3d6a4098d0c1e331
GuiMCL <113605222+GuiMCL@users.noreply.github.com>
2024-09-21 09:16:32 -0300

    1 commmit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Login from './SRC/pages/login';
+
+
+export default function App() {
+  return (
+        <Login/>
+  );
+}
